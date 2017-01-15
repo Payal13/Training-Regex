be575309ca83b64e2626b64bbd99e28536ce1942
Payal13 <payal@joshsoftware.com>
2017-01-16 01:22:19 +0530

    IP address validation program with regex #2

---
+++ IP_address_validation.rb
@@ -0,0 +1,12 @@
+#validating ip address ipv4 with Regex 
+def validate_ip_address(ip)
+  if ip =~ /([0-1][0-9]?[0-9]?|[2][0-4]?[0-9]?|[2][5]?[0-5]?|[3-9]?[0-9]?)\.([[0-1][0-9]?[0-9]?|[2][0-4]?[0-9]?|[2][5]?[0-5]?|[3-9]?[0-9]?)\.([0-1][0-9]?[0-9]?|[2][0-4]?[0-9]?|[2][5]?[0-5]?|[3-9]?[0-9]?)\.([0-1][0-9]?[0-9]?|[2][0-4]?[0-9]?|[2][5]?[0-5]?|[3-9]?[0-9]?)/
+    puts "valid ip address"
+  else
+    puts "invalid ip address"
+  end	
+end	
+
+puts "Enter any ip address for validation: "
+ip=gets.chomp
+validate_ip_address(ip)
